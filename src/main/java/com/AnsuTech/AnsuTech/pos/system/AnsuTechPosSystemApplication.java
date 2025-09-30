455351b5d982e6b55b68a7b826c3d8b77f3ca626
halfaoui <halfaouianis532@gmail.com>
2025-09-30 18:02:20 +0200

    first init

---
+++ src/main/java/com/AnsuTech/AnsuTech/pos/system/AnsuTechPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.AnsuTech.AnsuTech.pos.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsuTechPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsuTechPosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnsuTech/AnsuTech/pos/system/AnsuTechPosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnsuTech.AnsuTech.pos.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsuTechPosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
